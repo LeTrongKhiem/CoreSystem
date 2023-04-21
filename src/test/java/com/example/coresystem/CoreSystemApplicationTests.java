9789c02379f51d64222ef3d3fce546c561569931
LeTrongKhiem <19130102@st.hcmuaf.edu.vn>
2023-04-21 14:33:38 +0700

    base

---
+++ src/test/java/com/example/coresystem/CoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
