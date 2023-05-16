8f4b1a434f9b00dc593f4c237ba452b40b7b8607
TrangKwon <lptrangit@gmail.com>
2023-05-16 14:38:10 +0700

    send email service

--- BackEndAPI/src/main/java/com/example/backendapi/BackEndApiApplication.java
+++ BackEndAPI/src/main/java/com/example/backendapi/BackEndApiApplication.java
@@ -1,14 +1,20 @@
 package com.example.backendapi;
 
 import com.example.backendapi.Abstractions.IFileStorageService;
+import com.example.backendapi.Service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+
 
 @SpringBootApplication
 public class BackEndApiApplication {
-
+    @Autowired
+    private EmailService senderService;
     public static void main(String[] args) {
         SpringApplication.run(BackEndApiApplication.class, args);
     }
@@ -16,8 +22,13 @@ public class BackEndApiApplication {
 
     @Bean
     CommandLineRunner init(IFileStorageService storageService) {
-        return (args) -> {
-            storageService.init();
-        };
+        return (args) -> storageService.init();
+    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void triggerMail() {
+        senderService.sendSimpleEmail("trangphuonglam120@gmail.com",
+                "This is email body",
+                "This is email subject");
+
     }
 }
