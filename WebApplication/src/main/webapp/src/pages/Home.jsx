a33ae70b0fadcd43658e3da4b413371f3647d8fc
19130004 <19130004@st.hcmuaf.edu.vn>
2023-05-19 09:44:30 +0700

    hot fix multi images

--- WebApplication/src/main/webapp/src/pages/Home.jsx
+++ WebApplication/src/main/webapp/src/pages/Home.jsx
@@ -2,9 +2,13 @@ import Search from "../components/Search";
 import BookList from "../components/BookList";
 import {useCallback, useEffect, useState} from "react";
 import axios from "axios";
+import {Link, useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {selectIsLoggedIn} from "../redux/slice/authSlice";
+import {toast} from "react-toastify";
+
 const Home = () => {
     const [searchTerm, setSearchTerm] = useState('');
-    console.log(searchTerm)
     const [books, setBooks] = useState([]);
     console.log(books)
     const [sortBy, setSortBy] = useState('name');
@@ -13,9 +17,11 @@ const Home = () => {
     const [currentPage, setCurrentPage] = useState(0);
     const [totalPages, setTotalPages] = useState(0);
     const [pageSizes, setPageSizes] = useState(3);
+    const isLoggin = useSelector(selectIsLoggedIn);
+    const navigate = useNavigate()
 
-  useEffect(() => {
-      const getBooks = async () => {
+    useEffect(() => {
+        const getBooks = async () => {
             const response = await axios.get('http://localhost:7070/api/books/', {
                 params: {
                     search: searchTerm,
@@ -30,22 +36,34 @@ const Home = () => {
                 setBooks(response.data.results);
                 setTotalPages(response.data.totalPages);
             }
-          console.log(response.data)
-      }
+            console.log(response.data)
+        }
         getBooks();
-  },[searchTerm, sortBy, sortType, mostRecent, currentPage, pageSizes])
+    }, [searchTerm, sortBy, sortType, mostRecent, currentPage, pageSizes])
     const handleSearch = (searchTerm) => {
         setSearchTerm(searchTerm);
     };
     const handlePageChange = useCallback((page) => {
         setCurrentPage(page); //trừ 1 vì page bắt đầu từ 0
     }, []);
-
+    const goToPostBook = () => {
+        if (isLoggin) {
+            navigate('/post-book')
+        } else {
+            toast.error('Vui lòng đăng nhập để thực hiện chức năng này');
+            navigate('/login')
+        }
+    }
     return (
         <div className="home">
-            <Search searchTerm={searchTerm} handleSearch={handleSearch} />
-            <BookList books={books}  currentPage={currentPage} totalPages={totalPages} onChangePage={handlePageChange}/>
+            <div className="add-book">
+
+                <button className="btn btn-primary" onClick={goToPostBook}>Thêm sách</button>
+
+            </div>
+            <Search searchTerm={searchTerm} handleSearch={handleSearch}/>
+            <BookList books={books} currentPage={currentPage} totalPages={totalPages} onChangePage={handlePageChange}/>
         </div>
     );
 }
-export  default Home;
+export default Home;
